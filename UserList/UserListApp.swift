196e297b188e213945f0953af7380cd2a9b6a71a
Anton Kuznetsov <anton.kuznetsov.mail@gmail.com>
2025-06-18 13:30:38 +0200

    Initial Commit

---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by Anton Kuznetsov on 18.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
